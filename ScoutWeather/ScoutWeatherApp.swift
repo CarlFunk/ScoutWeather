549de8ea34ca2ee2b3ed5f8abafa78b4880a495a
Carl Funk <carl.funk@outlook.com>
2025-05-12 20:36:33 -0500

    Initial Commit

---
+++ ScoutWeather/ScoutWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutWeatherApp.swift
+//  ScoutWeather
+//
+//  Created by Carl Funk on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoutWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
